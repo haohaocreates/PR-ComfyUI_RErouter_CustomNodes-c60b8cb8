9f1db2298adcb371bd59972597e002dd77567ec8
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-27 23:46:27 +0300

    Update nodes_dev.py

--- nodes/nodes_dev.py
+++ nodes/nodes_dev.py
@@ -20,21 +20,33 @@ class test1:
     def reroute(self, STP_T, STP_R, STP_U):
         return (STP_T, STP_R, STP_U,)
 #====----Test2_DEV----====
-class test2:     
+class re_reroute:
 
     @classmethod
     def INPUT_TYPES(cls):
-               
-        return {"required": {       
-                    "int": ("INT", {"multiline": False, "default": "0"})
-                    
-                    }
-                }
+        return {
+            "required": {
+                "MODEL_B": ("MODEL",),
+                "CLIP_B": ("CLIP",),
+                "VAE_B": ("VAE",),
+                "MODEL_R": ("MODEL",),
+                "CLIP_R": ("CLIP",),
+                "VAE_R": ("VAE",),
+                "EM_LAT": ("LATENT",),
+                "UPSC": ("UPSCALE_MODEL",),
+                "STR_p": ("STRING", {"forceInput": True}),
+                "STR_n": ("STRING", {"forceInput": True}),
+                "SEED": ("INT", {"forceInput": True}),
+                "STP_T": ("INT", {"forceInput": True}),
+                "STP_R": ("INT", {"forceInput": True}),
+                "STP_U": ("INT", {"forceInput": True})
+            }
+        }
 
-    RETURN_TYPES = ("INT",)
-    FUNCTION = "int"
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "MODEL", "CLIP", "VAE", "LATENT", "UPSCALE_MODEL", "STRING", "STRING", "INT", "INT", "INT", "INT",)
+    RETURN_NAMES = ("MODEL_B", "CLIP_B", "VAE_B", "MODEL_R", "CLIP_R", "VAE_R", "EM_LAT", "UPSC", "STR_p", "STR_n", "SEED", "STP_T", "STP_R", "STP_U",)
+    FUNCTION = "reroute"
     CATEGORY = "(RE)route"
-
-    def concatenate_text(self, int):
-        
-        return (int,)
+	
+    def reroute(self, MODEL_B, CLIP_B, VAE_B, MODEL_R, CLIP_R, VAE_R, EM_LAT, UPSCALE_MODEL, STR_p, STR_n, SEED, STP_T, STP_R, STP_U):
+        return (MODEL_B, CLIP_B, VAE_B, MODEL_R, CLIP_R, VAE_R, EM_LAT, UPSCALE_MODEL, STR_p, STR_n, SEED, STP_T, STP_R, STP_U,)
