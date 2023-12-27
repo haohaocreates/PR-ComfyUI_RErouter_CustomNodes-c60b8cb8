8bac80fa79be3841f0c3541182eaac05dec0ad74
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-27 20:40:08 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from .nodes.nodes import *
-from .nodes.nodes_dev import *
+#from .nodes.nodes_dev import *
 
 NODE_CLASS_MAPPINGS = {
-    "Rrt": reroute_rrt,
-    "String Input": string_input,
-    "Test DEV": test1,
-    "Test2 DEV": test2,
+    "Reroute (RE)": re_reroute,
+    "String (RE)": re_string,
+#    "Test DEV": test1,
+#    "Test2 DEV": test2,
     }
     
 print("\033[34mComfyUI REroute Nodes: \033[92mLoaded\033[0m")
