764dbc4cfdc521a4173b1f8196d48f39a50d20ec
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-27 20:37:13 +0300

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -1,37 +1,46 @@
-#====----REroute----====
-class reroute_rrt:
+#====----REroute_RE----====
+class re_reroute:
 
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "MODEL": ("MODEL",),
-                "CLIP": ("CLIP",),
-                "VAE": ("VAE",),
+                "MODEL_B": ("MODEL",),
+                "CLIP_B": ("CLIP",),
+                "VAE_B": ("VAE",),
+                "MODEL_R": ("MODEL",),
+                "CLIP_R": ("CLIP",),
+                "VAE_R": ("VAE",),
+                "EM_LAT": ("LATENT",),
                 "UPSC": ("UPSCALE_MODEL",),
                 "STR_p": ("STRING", {"forceInput": True}),
                 "STR_n": ("STRING", {"forceInput": True}),
-                "SEED": ("INT", {"forceInput": True})
+                "SEED": ("INT", {"forceInput": True}),
+                "STP_T": ("INT", {"forceInput": True}),
+                "STP_R": ("INT", {"forceInput": True}),
+                "STP_U": ("INT", {"forceInput": True})
             }
         }
 
-    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "UPSCALE_MODEL", "STRING", "STRING", "INT",)
-    RETURN_NAMES = ("MODEL", "CLIP", "VAE", "UPSC", "STR_p", "STR_n", "SEED",)
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "MODEL", "CLIP", "VAE", "LATENT", "UPSCALE_MODEL", "STRING", "STRING", "INT", "INT", "INT", "INT",)
+    RETURN_NAMES = ("MODEL_B", "CLIP_B", "VAE_B", "MODEL_R", "CLIP_R", "VAE_R", "EM_LAT", "UPSC", "STR_p", "STR_n", "SEED", "STP_T", "STP_R", "STP_U",)
     FUNCTION = "reroute"
-    CATEGORY = "REroute Nodes/Test"
+    CATEGORY = "(RE)route"
 	
-    def reroute(self, MODEL, CLIP, VAE, UPSCALE_MODEL, STR_p, STR_n, SEED):
-        return (MODEL, CLIP, VAE, UPSCALE_MODEL, STR_p, STR_n, SEED,)
-#====----String_Input----====
-class string_input:
+    def reroute(self, MODEL_B, CLIP_B, VAE_B, MODEL_R, CLIP_R, VAE_R, EM_LAT, UPSCALE_MODEL, STR_p, STR_n, SEED, STP_T, STP_R, STP_U):
+        return (MODEL_B, CLIP_B, VAE_B, MODEL_R, CLIP_R, VAE_R, EM_LAT, UPSCALE_MODEL, STR_p, STR_n, SEED, STP_T, STP_R, STP_U,)
+
+
+#====----String_RE----====
+class re_string:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"prompt": ("STRING", {"default": "prompt", "multiline": True})}}
+        return {"required": {"prompt": ("STRING", {"default": "", "multiline": True})}}
 
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("prompt",)
     FUNCTION = "get_value"
-    CATEGORY = "REroute Nodes"
+    CATEGORY = "(RE)route"
 
     def get_value(self, prompt):
         return (prompt,)        
