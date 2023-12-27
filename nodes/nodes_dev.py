da5543f491fa12638fe91aa82f5655db3985573c
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-27 14:21:54 +0300

    Update nodes_dev.py

--- nodes/nodes_dev.py
+++ nodes/nodes_dev.py
@@ -1,5 +1,5 @@
-#====----Test_node----====
-class test:
+#====----Test1_DEV----====
+class test1:
 
     @classmethod
     def INPUT_TYPES(s):
@@ -15,4 +15,25 @@ class test:
     CATEGORY = "1Girl Nodes"
 
     def reroute(self, STR_p):
-        return (STR_p,)   
+        return (STR_p,)
+#====----Test2_DEV----====
+class test2:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "MODEL": ("MODEL",),
+                "CLIP": ("CLIP",),
+                "VAE": ("VAE",),
+                "UPSC": ("UPSCALE_MODEL",)
+            }
+        }
+
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "UPSCALE_MODEL",)
+    RETURN_NAMES = ("MODEL", "CLIP", "VAE", "UPSC",)
+    FUNCTION = "reroute"
+    CATEGORY = "1Girl Nodes"
+
+    def reroute(self, MODEL, CLIP, VAE, UPSCALE_MODEL):
+	return (MODEL, CLIP, VAE, UPSCALE_MODEL,)
