88107188c9d74a5cd20d5ec233f910860b199bf3
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-27 18:33:59 +0300

    Update nodes_dev.py

--- nodes/nodes_dev.py
+++ nodes/nodes_dev.py
@@ -1,21 +1,26 @@
 #====----Test1_DEV----====
-class test1:
+class reroute_rrt:
 
     @classmethod
-    def INPUT_TYPES(s):
+    def INPUT_TYPES(cls):
         return {
             "required": {
-                "STR_p": ("STRING", {"forceInput": True})
+                "MODEL": ("MODEL",),
+                "CLIP": ("CLIP",),
+                "VAE": ("VAE",),
+                "UPSC": ("UPSCALE_MODEL",),
+                "STR_p": ("STRING", {"forceInput": True}),
+                "STR_n": ("STRING", {"forceInput": True})
             }
         }
 
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("STR_p",)
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "UPSCALE_MODEL", "STRING", "STRING",)
+    RETURN_NAMES = ("MODEL", "CLIP", "VAE", "UPSC", "STR_p", "STR_n",)
     FUNCTION = "reroute"
     CATEGORY = "REroute Nodes/Test"
-
-    def reroute(self, STR_p):
-        return (STR_p,)
+	
+    def reroute(self, MODEL, CLIP, VAE, UPSCALE_MODEL, STR_p, STR_n):
+        return (MODEL, CLIP, VAE, UPSCALE_MODEL, STR_p, STR_n,)
 #====----Test2_DEV----====
 class test2:
 
