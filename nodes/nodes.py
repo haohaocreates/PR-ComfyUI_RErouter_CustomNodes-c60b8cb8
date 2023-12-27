86a91db95b87aa755f8f376c687058167d0ebfb7
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-27 22:24:41 +0300

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -35,12 +35,31 @@ class re_reroute:
 class re_string:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"prompt": ("STRING", {"default": "", "multiline": True})}}
+        return {"required": {"string": ("STRING", {"default": "", "multiline": True})}}
 
     RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("prompt",)
-    FUNCTION = "get_value"
+    RETURN_NAMES = ("string",)
+    FUNCTION = "get_string"
     CATEGORY = "(RE)route"
 
-    def get_value(self, prompt):
-        return (prompt,)        
+    def get_string(self, string):
+        return (string,)        
+#====----Int_RE----====
+class re_int:     
+
+    @classmethod
+    def INPUT_TYPES(cls):
+               
+        return {"required": {       
+                    "int": ("INT", {"multiline": False, "default": "0"})
+                    
+                    }
+                }
+
+    RETURN_TYPES = ("INT",)
+    FUNCTION = "get_int"
+    CATEGORY = "(RE)route"
+
+    def get_int(self, int):
+        
+        return (int,)
