0773715158652cd4eb0e5f566ccac9dbda16785a
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-27 22:25:47 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from .nodes.nodes_dev import *
 NODE_CLASS_MAPPINGS = {
     "Reroute (RE)": re_reroute,
     "String (RE)": re_string,
+    "INT (RE)": re_int,
     "Test1 DEV": test1,
     "Test2 DEV": test2,
     }
