90e70ac9dcf9c1bfaab3bc8c03014004dd352ddb
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-29 00:58:53 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
 from .nodes.nodes import *
 from .nodes.rerouter import *
-#from .nodes.nodes_dev import *
 
 NODE_CLASS_MAPPINGS = {
     "RErouter =>": re_router_pack,
     "RErouter <=": re_router_unpack,
     "String (RE)": re_string,
     "Int (RE)": re_int
-#    "Test1 DEV": test1,
-#    "Test2 DEV": test2,
+
     }
     
-print("\033[34mComfyUI REroute Nodes: \033[92mLoaded\033[0m")
