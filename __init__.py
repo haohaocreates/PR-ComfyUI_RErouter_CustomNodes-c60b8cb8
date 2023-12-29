aff8dd5358d112c7853ce6e2ad2ff96e695d09dd
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-29 17:22:23 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from .nodes.nodes import *
 from .nodes.rerouter import *
+from .nodes.cliptextencode import *
 
 NODE_CLASS_MAPPINGS = {
     "RErouter =>": re_router_pack,
     "RErouter <=": re_router_unpack,
     "String (RE)": re_string,
-    "Int (RE)": re_int
+    "Int (RE)": re_int,
+    "CLIPTextEncode (RE)": CLIPTextEncode,
+    "CLIPTextEncodeSDXL (RE)": CLIPTextEncodeSDXL,
+    "CLIPTextEncodeSDXLRefiner": CLIPTextEncodeSDXLRefiner
 
     }
     
