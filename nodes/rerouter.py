453d509310cac77fe97d1c2534a71aa9b3f4b5fb
an90ray <154269455+an90ray@users.noreply.github.com>
2023-12-29 19:54:10 +0300

    Update rerouter.py

--- nodes/rerouter.py
+++ nodes/rerouter.py
@@ -19,15 +19,16 @@ class re_router_pack:
                 "SEED": ("INT", {"forceInput": True}),
                 "INT1": ("INT", {"forceInput": True}),
                 "INT2": ("INT", {"forceInput": True}),
-                "INT3": ("INT", {"forceInput": True})}}
+                "INT3": ("INT", {"forceInput": True}),
+	    	"STR1": ("STRING", {"forceInput": True})}}
 
     RETURN_TYPES = ("BUS_BASE",)
     RETURN_NAMES = ("bus_",)
     FUNCTION = "re_pack"
     CATEGORY = "(RE)router"
 	
-    def re_pack(self, MODEL, CLIP, VAE, MODEL_R=None, CLIP_R=None, VAE_R=None, EMPT_LAT=None, UPSC=None, PRT_p=None, PRT_n=None, SEED=None, INT1=None, INT2=None, INT3=None):
-        return ((MODEL, CLIP, VAE, MODEL_R, CLIP_R, VAE_R, EMPT_LAT, UPSC, PRT_p, PRT_n, SEED, INT1, INT2, INT3),)
+    def re_pack(self, MODEL, CLIP, VAE, MODEL_R=None, CLIP_R=None, VAE_R=None, EMPT_LAT=None, UPSC=None, PRT_p=None, PRT_n=None, SEED=None, INT1=None, INT2=None, INT3=None, STR1=None,):
+        return ((MODEL, CLIP, VAE, MODEL_R, CLIP_R, VAE_R, EMPT_LAT, UPSC, PRT_p, PRT_n, SEED, INT1, INT2, INT3, STR1),)
 
 #====----(RE) _route----====
 class re_router_unpack:
@@ -37,10 +38,10 @@ class re_router_unpack:
         return {
             "required": {"_bus": ("BUS_BASE",)}}
             
-    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "MODEL", "CLIP", "VAE", "LATENT", "UPSCALE_MODEL", "STRING", "STRING", "INT", "INT", "INT", "INT",)
-    RETURN_NAMES = ("MODEL", "CLIP", "VAE", "MODEL_R", "CLIP_R", "VAE_R", "EMPT_LAT", "UPSC", "PRT_p", "PRT_n", "SEED", "INT1", "INT2", "INT3",)
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "MODEL", "CLIP", "VAE", "LATENT", "UPSCALE_MODEL", "STRING", "STRING", "INT", "INT", "INT", "INT", "STRING",)
+    RETURN_NAMES = ("MODEL", "CLIP", "VAE", "MODEL_R", "CLIP_R", "VAE_R", "EMPT_LAT", "UPSC", "PRT_p", "PRT_n", "SEED", "INT1", "INT2", "INT3", "STR1",)
     FUNCTION = "re_unpack"
     CATEGORY = "(RE)router"
     
     def re_unpack(self, _bus):
-        return (_bus[0], _bus[1], _bus[2], _bus[3], _bus[4], _bus[5], _bus[6], _bus[7], _bus[8], _bus[9], _bus[10], _bus[11], _bus[12], _bus[13],)
+        return (_bus[0], _bus[1], _bus[2], _bus[3], _bus[4], _bus[5], _bus[6], _bus[7], _bus[8], _bus[9], _bus[10], _bus[11], _bus[12], _bus[13], _bus[14],)
